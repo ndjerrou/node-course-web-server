af4c43eab17021b0a44c9fae862556c7959505f9
ndjerrou <n.djerrou@gmail.com>
2019-01-22 11:31:26 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+
+const app = express();
+
+hbs.registerPartials(__dirname+ '/views/partials')
+app.set('view engine', 'hbs')
+
+
+app.use((req, res, next) => {
+    const now = new Date().toString(); //human readable date
+    const log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log)
+    fs.appendFile('server-log', log+'\n', (err) => {
+        if (err){
+            console.log('Unable to append to server.log')
+        }
+    })
+    next();
+})
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs')
+})
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear() )
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+    res.render('home.hbs',{
+        pageTitle: 'Home page',
+        welcomeMessage: "Bienvenue sur mon site"
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About page',
+    })
+})
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: "Unable to understand your choice of navigation"
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000. PLease visite http://localhost:3000')
+})
